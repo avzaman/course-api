b9ede4d756281f3878431d1c7c6340cf0c10d6a3
Vincent Zaman Admin <vzaman-admin@ms-dca-domain.local>
2025-06-02 12:14:09 -0400

    made /hello REST controller -Vince

--- src/main/java/avzaman/CourseApiApp.java
+++ src/main/java/avzaman/CourseApiApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseApiApp {
     public static void main(String[] args) {
+        // sets default config, starts spring app context, class path scan, starts Tomcat server
         SpringApplication.run(CourseApiApp.class, args);
 
     }
---
+++ src/main/java/avzaman/HelloController.java
@@ -0,0 +1,13 @@
+package avzaman;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/hello")
+    public String requestMethodName() {
+        return "Hi";
+    }
+}
